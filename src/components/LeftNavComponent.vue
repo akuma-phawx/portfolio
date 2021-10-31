eaadf3a114cf8bb5dc5a42db7fea718510bb3239
Christoforos Vradis <chrisvradis00@gmail.com>
2021-10-31 16:51:35 +0200

    Add funy icon on mobile devices nav menu

--- src/components/LeftNavComponent.vue
+++ src/components/LeftNavComponent.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="left-section flex">
+    <div class="logo-section sm:hidden">
+      <img
+        src="../../public/psyduck.png"
+        class="object-contain h-8 ml-6"
+        alt=""
+      />
+    </div>
     <div class="link_container hidden sm:block ml-8 mt-1">
       <router-link class="nav-link" to="/">Home</router-link>
       <router-link class="nav-link" to="/about">About</router-link>
