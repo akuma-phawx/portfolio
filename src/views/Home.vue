ecb244336a7ee265342781c37b33d1e7066542cb
Christoforos Vradis <chrisvradis00@gmail.com>
2021-10-31 16:37:47 +0200

    Custom home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,45 @@
 <template>
-  <div id="home" class="container flex justify-center max-w-full">
-    <h1>Home</h1>
+  <div id="home" class="container flex flex-col justify-center max-w-full">
+    <!-- Text -->
+    <div class="mt-32 sm:mt-64">
+      <h1 class="home-intro text-4xl md:text-5xl lg:text-7xl">
+        <div class="upper-text">
+          <span class="font-black">&lt;</span>
+          <span class="text-shadow md:text-shadow-md lg:text-shadow-lg">
+            Hi, I'm Christopher Vradis</span
+          >
+        </div>
+        <div class="lower-text mt-4">
+          <span class="text-shadow md:text-shadow-md lg:text-shadow-lg"
+            >Full Stack Developer from Greece
+          </span>
+          <span class="font-black">/</span>
+          <span class="font-black">&gt;</span>
+        </div>
+      </h1>
+    </div>
+    <!-- Button -->
+    <div class="mx-auto mt-16">
+      <a
+        href="#"
+        class="
+          bg-transparent
+          border border-black
+          text-black
+          hover:bg-black hover:text-white
+          text-center text-xl
+          sm:text-2xl
+          py-2
+          px-4
+          rounded
+          w-max
+          mt-8
+          mx-auto
+        "
+      >
+        Here's my work
+      </a>
+    </div>
   </div>
 </template>
 
